18cab7a4bd9e2c7e0c340f3f3691aab26ae2f125
ale-ja12 <alejandram.120609@gmail.com>
2025-09-13 10:56:20 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -5,4 +5,9 @@ $producto2 = $_POST ['Producto 2'];
 $precio2 = $_POST ['Precio 2'];
 $producto3 = $_POST ['Producto 3'];
 $precio3 = $_POST ['Precio 3'];
+
+echo "<h2>Información recibida de los productos</h2>";
+$a = array($precio1, $precio2, $precio3);
+echo "sum(a) = " . array_sum($a) . "\n";
+
 ?>
