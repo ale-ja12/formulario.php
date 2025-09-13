cad25c8ebc488f559b8cfe51e9cc5421f3df22d3
ale-ja12 <alejandram.120609@gmail.com>
2025-09-13 10:52:23 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,3 +1,8 @@
 <?php
-$producto
+$producto1 = $_POST ['Producto 1'];
+$precio1 = $_POST ['Precio 1'];
+$producto2 = $_POST ['Producto 2'];
+$precio2 = $_POST ['Precio 2'];
+$producto3 = $_POST ['Producto 3'];
+$precio3 = $_POST ['Precio 3'];
 ?>
