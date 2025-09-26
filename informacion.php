4f037ecbfd3710178df630789d6a503c16f975a4
ale-ja12 <alejandram.120609@gmail.com>
2025-09-26 18:39:37 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -7,7 +7,6 @@ $producto3 = $_POST ['Producto 3'];
 $precio3 = $_POST ['Precio 3'];
 
 echo "<h2>Información recibida de los productos</h2>";
-$suma = ($precio1 + $precio2 + $precio3);
-echo "El total de la suma es" .$suma.;
-
+$suma = $precio1 + $precio2 + $precio3;
+echo "El total de la suma es" .$suma;
 ?>
